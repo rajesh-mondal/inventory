5766ee2e1dbdbb1b39660be030c82a5138f91d97
Rajesh Mondal <rajesh01749@gmail.com>
2024-01-01 22:58:10 +0600

    sending otp code to email - password recover stage 01 (end point)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Helper\JWTToken;
+use App\Mail\OTPMail;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller 
 {
@@ -44,6 +46,29 @@ class UserController extends Controller
                 'status'  => 'success',
                 'message' => 'User Login Successful',
                 'token'   => $token,
+            ], 200 );
+        } else {
+            return response()->json( [
+                'status'  => 'failed',
+                'message' => 'unauthorized',
+            ], 200 );
+        }
+    }
+
+    function SendOtpCode( Request $request ) {
+        $email = $request->input( 'email' );
+        $otp = rand( 1000, 9999 );
+        $count = User::where( 'email', '=', $email )->count();
+
+        if ( $count == 1 ) {
+            // OTP Send to Email Address
+            Mail::to( $email )->send( new OTPMail( $otp ) );
+            // OTP Code Insert to Table
+            User::where( 'email', '=', $email )->update( ['otp' => $otp] );
+
+            return response()->json( [
+                'status'  => 'success',
+                'message' => '4 digit OTP code has been send to your email!',
             ], 200 );
         } else {
             return response()->json( [
