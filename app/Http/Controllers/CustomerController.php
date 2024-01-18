e0dc4ce495abb88bf1275c56652b3d83e9b1844f
Rajesh Mondal <rajesh01749@gmail.com>
2024-01-18 13:29:28 +0600

    customer create and listing (back-end)

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    function CustomerPage() {
+        return view( 'pages.dashboard.customer-page' );
+    }
+
+    function CustomerList( Request $request ) {
+        $user_id = $request->header( 'id' );
+        return Customer::where( 'user_id', $user_id )->get();
+    }
+
+    function CustomerCreate( Request $request ) {
+        $user_id = $request->header( 'id' );
+        return Customer::create( [
+            'name'    => $request->input( 'name' ),
+            'email'    => $request->input( 'email' ),
+            'mobile'    => $request->input( 'mobile' ),
+            'user_id' => $user_id,
+        ] );
+    }
 }
