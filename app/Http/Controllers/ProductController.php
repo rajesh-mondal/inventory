5ba5a1f1606603c8d573dc5411515ea8e945fdf5
Rajesh Mondal <rajesh01749@gmail.com>
2024-01-20 20:28:17 +0600

    product update with and without image (backend)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -53,4 +53,40 @@ class ProductController extends Controller {
         $user_id = $request->header( 'id' );
         return Product::where( 'user_id', $user_id )->get();
     }
+
+    function UpdateProduct( Request $request ) {
+        $user_id = $request->header( 'id' );
+        $product_id = $request->input( 'id' );
+
+        if ( $request->hasFile( 'img' ) ) {
+            // Upload New File
+            $img = $request->file( 'img' );
+            $t = time();
+            $file_name = $img->getClientOriginalName();
+            $img_name = "{$user_id}-{$t}-{$file_name}";
+            $img_url = "uploads/{$img_name}";
+            $img->move( public_path( 'uploads' ), $img_name );
+
+            // Delete Old File
+            $filePath = $request->input( 'file_path' );
+            File::delete( $filePath );
+
+            // Update Product
+            return Product::where( 'id', $product_id )->where( 'user_id', $user_id )->update( [
+                'name'        => $request->input( 'name' ),
+                'price'       => $request->input( 'price' ),
+                'unit'        => $request->input( 'unit' ),
+                'img_url'     => $img_url,
+                'category_id' => $request->input( 'category_id' ),
+                'user_id'     => $user_id,
+            ] );
+        } else {
+            return Product::where( 'id', $product_id )->where( 'user_id', $user_id )->update( [
+                'name'        => $request->input( 'name' ),
+                'price'       => $request->input( 'price' ),
+                'unit'        => $request->input( 'unit' ),
+                'category_id' => $request->input( 'category_id' ),
+            ] );
+        }
+    }
 }
