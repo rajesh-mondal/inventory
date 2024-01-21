dbc5b2379d2610865691da8aeb7ec8fd058fe203
Rajesh Mondal <rajesh01749@gmail.com>
2024-01-21 22:13:47 +0600

    sidebar menu added in admin dashboard (front end)

--- resources/views/layout/sidenav-layout.blade.php
+++ resources/views/layout/sidenav-layout.blade.php
@@ -67,14 +67,45 @@
 </nav>
 
 <div id="sideNavRef" class="side-nav-open">
+    <a href="{{url("/dashboard")}}" class="side-bar-item">
+        <i class="bi bi-graph-up"></i>
+        <span class="side-bar-item-caption">Dashboard</span>
+    </a>
 
+    <a href="{{url("/customerPage")}}" class="side-bar-item">
+        <i class="bi bi-people"></i>
+        <span class="side-bar-item-caption">Customer</span>
+    </a>
+
+    <a href="{{url("/categoryPage")}}" class="side-bar-item">
+        <i class="bi bi-list-nested"></i>
+        <span class="side-bar-item-caption">Category</span>
+    </a>
+
+    <a href="{{url("/productPage")}}" class="side-bar-item">
+        <i class="bi bi-bag"></i>
+        <span class="side-bar-item-caption">Product</span>
+    </a>
+
+    <a href="{{url('/salePage')}}" class="side-bar-item">
+        <i class="bi bi-currency-dollar"></i>
+        <span class="side-bar-item-caption">Create Sale</span>
+    </a>
+
+    <a href="{{url('/invoicePage')}}" class="side-bar-item">
+        <i class="bi bi-receipt"></i>
+        <span class="side-bar-item-caption">Invoice</span>
+    </a>
+
+    <a href="{{url('/reportPage')}}" class="side-bar-item">
+        <i class="bi bi-file-earmark-bar-graph"></i>
+        <span class="side-bar-item-caption">Report</span>
+    </a>
 </div>
 
 <div id="contentRef" class="content">
     @yield('content')
 </div>
-
-
 
 <script>
     function MenuBarClickHandler() {
