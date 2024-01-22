deff521d5df163b555cd72f27b2f692032c278e8
Rajesh Mondal <rajesh01749@gmail.com>
2024-01-22 22:03:19 +0600

    product create (front end)

--- resources/views/components/product/product-create.blade.php
+++ resources/views/components/product/product-create.blade.php
@@ -24,12 +24,13 @@
                                 <label class="form-label mt-2">Unit</label>
                                 <input type="text" class="form-control" id="productUnit">
 
-                                <br/>
-                                <img class="w-15" id="newImg" src="{{asset('images/default.jpg')}}"/>
-                                <br/>
+                                <br />
+                                <img class="w-15" id="newImg" src="{{ asset('images/default.jpg') }}" />
+                                <br />
 
                                 <label class="form-label">Image</label>
-                                <input oninput="newImg.src=window.URL.createObjectURL(this.files[0])" type="file" class="form-control" id="productImg">
+                                <input oninput="newImg.src=window.URL.createObjectURL(this.files[0])" type="file"
+                                    class="form-control" id="productImg">
 
                             </div>
                         </div>
@@ -37,8 +38,9 @@
                 </form>
             </div>
             <div class="modal-footer">
-                <button id="modal-close" class="btn bg-gradient-primary mx-2" data-bs-dismiss="modal" aria-label="Close">Close</button>
-                <button onclick="Save()" id="save-btn" class="btn bg-gradient-success" >Save</button>
+                <button id="modal-close" class="btn bg-gradient-primary mx-2" data-bs-dismiss="modal"
+                    aria-label="Close">Close</button>
+                <button onclick="Save()" id="save-btn" class="btn bg-gradient-success">Save</button>
             </div>
         </div>
     </div>
@@ -46,5 +48,60 @@
 
 
 <script>
+    FillCategoryDropDown();
 
+    async function FillCategoryDropDown() {
+        let res = await axios.get("/list-category")
+        res.data.forEach(function(item, i) {
+            let option = `<option value="${item['id']}">${item['name']}</option>`
+            $("#productCategory").append(option);
+        })
+    }
+
+    async function Save() {
+        let productCategory = document.getElementById('productCategory').value;
+        let productName = document.getElementById('productName').value;
+        let productPrice = document.getElementById('productPrice').value;
+        let productUnit = document.getElementById('productUnit').value;
+        let productImg = document.getElementById('productImg').files[0];
+
+        if (productCategory.length === 0) {
+            errorToast("Product Category Required !")
+        } else if (productName.length === 0) {
+            errorToast("Product Name Required !")
+        } else if (productPrice.length === 0) {
+            errorToast("Product Price Required !")
+        } else if (productUnit.length === 0) {
+            errorToast("Product Unit Required !")
+        } else if (!productImg) {
+            errorToast("Product Image Required !")
+        } else {
+            document.getElementById('modal-close').click();
+
+            let formData = new FormData();
+            formData.append('img', productImg)
+            formData.append('name', productName)
+            formData.append('price', productPrice)
+            formData.append('unit', productUnit)
+            formData.append('category_id', productCategory)
+
+            const config = {
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            }
+
+            showLoader();
+            let res = await axios.post("/create-product", formData, config)
+            hideLoader();
+
+            if (res.status === 201) {
+                successToast('Request completed');
+                document.getElementById("save-form").reset();
+                await getList();
+            } else {
+                errorToast("Request fail !")
+            }
+        }
+    }
 </script>
